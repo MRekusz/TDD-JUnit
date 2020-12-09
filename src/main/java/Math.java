d110b4cdff2b8426917840934183618380167165
MRekusz <maciek-x2@o2.pl>
2020-12-09 19:44:01 +0100

    tests

---
+++ src/main/java/Math.java
@@ -0,0 +1,48 @@
+public class Math {
+
+    public static int findMax(int[] numbers) {
+        int max = Integer.MAX_VALUE;
+        for (int number : numbers) {
+            max = number;
+        }
+        return max;
+    }
+
+    public static int sumCorrect(float[] numbers) {
+        int sum = 0;
+        for (float number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static float Average(float[] numbers) {
+
+        return sumCorrect(numbers) / (float) numbers.length;
+    }
+
+    public static int factorial(int i) {
+        if (i < 1)
+            return 1;
+        else
+            return i * factorial(i - 1);
+    }
+
+    public static int multiplyArray(int[] numbers) {
+        int ratio = 1;
+        for (int number : numbers) {
+            ratio *= number;
+        }
+        return ratio;
+    }
+
+    public static int findMin(int[] numbers) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < numbers.length; i++) {
+            if (min > numbers[i]) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+}
